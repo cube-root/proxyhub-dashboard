814e90ab46c21cdb7b9311d6993c6d34ade8d974
Abhijith V <abhijithababhijith@gmail.com>
2025-01-24 23:18:09 +0530

    nextconfig updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production'
+  },
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ['lucide-react', '@next/third-parties'],
+    webpackBuildWorker: true
+  },
+  poweredByHeader: false,
+  compress: true
 };
 
 export default nextConfig;
